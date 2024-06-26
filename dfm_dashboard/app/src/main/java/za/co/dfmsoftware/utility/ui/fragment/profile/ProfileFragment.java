43d9028ea77275409ca60f320d281dde59eb801d
DfmNehemiah <nehemiah@dfmsoftware.co.za>
2024-06-26 14:45:33 +0200

    logout and delete account elements added to fragment profile

--- dfm_dashboard/app/src/main/java/za/co/dfmsoftware/utility/ui/fragment/profile/ProfileFragment.java
+++ dfm_dashboard/app/src/main/java/za/co/dfmsoftware/utility/ui/fragment/profile/ProfileFragment.java
@@ -1,26 +1,20 @@
 package za.co.dfmsoftware.utility.ui.fragment.profile;
 
-import android.content.Intent;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.view.MenuProvider;
-import androidx.lifecycle.Lifecycle;
 
 import za.co.dfmsoftware.utility.R;
-import za.co.dfmsoftware.utility.realm.DfmRealm;
 import za.co.dfmsoftware.utility.ui.base.BaseFragment;
-import za.co.dfmsoftware.utility.ui.login.LoginActivity;
 
 public class ProfileFragment extends BaseFragment<ProfileContract.Presenter, ProfileContract.View> implements ProfileContract.View {
 
     private TextView emailAddressTextView;
     private ProfileContract.Presenter presenter;
+    private Button logoutButton;
+    private Button deleteAccountButton;
 
     @Override
     protected int getLayoutResId() { return R.layout.fragment_profile; }
@@ -37,20 +31,17 @@ public class ProfileFragment extends BaseFragment<ProfileContract.Presenter, Pro
     @Override
     protected void setupViews(View view) {
         this.emailAddressTextView = view.findViewById(R.id.email_address_textview);
+        this.logoutButton = view.findViewById(R.id.logout_button);
+        this.deleteAccountButton = view.findViewById(R.id.delete_account_button);
         this.presenter.init();
 
-        //set up MenuProvider
-        requireActivity().addMenuProvider(new MenuProvider() {
-            @Override
-            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
-                menuInflater.inflate(R.menu.profile_action_menu, menu);
-            }
+        logoutButton.setOnClickListener(v -> {
+            System.out.println("Start logout intent");
+        });
 
-            @Override
-            public boolean onMenuItemSelected(MenuItem menuItem) {
-                return handleMenuItemClick(menuItem);
-            }
-        }, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
+        deleteAccountButton.setOnClickListener(v -> {
+            System.out.println("Start delete account intent");
+        });
     }
 
     @Override
@@ -59,23 +50,6 @@ public class ProfileFragment extends BaseFragment<ProfileContract.Presenter, Pro
     @Override
     public void setEmailAddress(@Nullable String emailAddress) {
         this.emailAddressTextView.setText(emailAddress);
-    }
-
-    private boolean handleMenuItemClick(MenuItem item) {
-        int id = item.getItemId();
-        if(id == R.id.logout_button) {
-            DfmRealm dfmRealm = this.getDfmRealm();
-            dfmRealm.clearAllData();
-
-            Intent loginIntent = new Intent(this.getActivity(), LoginActivity.class);
-            loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            this.startActivity(loginIntent);
-            this.getActivity().finish();
-
-            return true;
-        }
-
-        return false;
     }
 }
 
